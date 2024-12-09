11433439b26b012f0d1b909b848f0138f1f5bd79
Tanaratta Umpon <tanaratta@gmail.com>
2024-12-09 12:18:15 +0700

    first commit

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Date/Coding_Exercise_01-02.py
@@ -0,0 +1,16 @@
+#   Exercise 01 - Printing Practice
+#   Write a program that uses print statements to print the following recipe into the Output console
+#   The text to print is already there, you just need to make it into code.
+#   Your code should print all five lines exactly the same as the example output below.
+#   Make sure that you don't change any of these existing text as everything, punctuation and casing all need to match!
+
+#   Exercise 02 - Debugging Practice
+#   Look at the code in the code editor. 
+#   There are errors on all 5 lines of code. Fix the code so that it runs without errors.
+#   Try to run the code and debug each line using the error messages and feedback.
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
