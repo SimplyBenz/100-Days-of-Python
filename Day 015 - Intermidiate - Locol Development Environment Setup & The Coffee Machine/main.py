47ecf00ce14580844c114abbcabbf002c0a4540d
Tanaratta Umpon <tanaratta@gmail.com>
2025-01-03 16:26:02 +0700

    Day 15 of python

--- Day 015 - Intermidiate - Locol Development Environment Setup & The Coffee Machine/main.py
+++ Day 015 - Intermidiate - Locol Development Environment Setup & The Coffee Machine/main.py
@@ -1,58 +1,69 @@
 from coffe_recipe import MENU
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "water": 3000,
+    "milk": 2000,
+    "coffee": 1000,
     "money" : 0,
 }
+
+coffee_type = ""
 
 #TODO Prompt user by asking “What would you like? (espresso/latte/cappuccino):"
 def start_order():
     
+    global coffee_type
     coffee_type = input("What would you like? (espresso/latte/cappuccino): ")
 
     #TODO Turn off the Coffee Machine by entering “off” to the prompt. coffee machine should have secret code that is required to turn off.
     if coffee_type == "off":
-        secret_code = input("Enter the secret code: ")
-        if secret_code == "123":
-            print("Turning off the coffee machine.")
-            exit()
-        else:
-            print("Invalid secret code! Try again.")
-
-    #TODO Print report. eg. Water: 100ml Milk: 50ml Coffee: 76g Money: $2.5
-    if coffee_type == "report":
-        print(f"Water: {resources["water"]}ml\nMilk: {resources["milk"]}ml\nCoffee: {resources["coffee"]}g\nMoney: ${resources["money"]}")
-
-    if coffee_type not in MENU:
-        print("Invalid coffee type! Try again.")
+        print("Turning off the coffee machine.")
         exit()
 
-    if coffee_type == "espresso":
-        WaterUSE = 50
-        CoffeeUSE = 18
-        MoneyUSE = 1.5
-    elif coffee_type == "latte":
-        WaterUSE = 200
-        MilkUSE = 150
-        CoffeeUSE = 24
-        MoneyUSE = 2.5
-    elif coffee_type == "cappuccino":
-        WaterUSE = 250
-        MilkUSE = 100
-        CoffeeUSE = 24
-        MoneyUSE = 3.0
+    #TODO Print report. eg. Water: 100ml Milk: 50ml Coffee: 76g Money: $2.5
+    elif coffee_type == "report":
+        print(f"Water: {resources["water"]}ml\nMilk: {resources["milk"]}ml\nCoffee: {resources["coffee"]}g\nMoney: ${resources["money"]}")
+        start_order()
+
+    elif coffee_type in MENU:
+        print("In the menu.")
+        if coffee_type == "espresso":
+            WaterUSE = MENU["espresso"]["ingredients"].get("water")
+            MilkUSE = MENU["espresso"]["ingredients"].get("milk",0)
+            CoffeeUSE = MENU["espresso"]["ingredients"].get("coffee")
+            MoneyUSE = MENU["espresso"]["cost"]
+        elif coffee_type == "latte":
+            WaterUSE = MENU["latte"]["ingredients"].get("water")
+            MilkUSE = MENU["latte"]["ingredients"].get("water")
+            CoffeeUSE = MENU["latte"]["ingredients"].get("water")
+            MoneyUSE = MENU["latte"]["cost"]
+        elif coffee_type == "cappuccino":
+            WaterUSE = MENU["cappuccino"]["ingredients"].get("water")
+            MilkUSE = MENU["cappuccino"]["ingredients"].get("milk")
+            CoffeeUSE = MENU["cappuccino"]["ingredients"].get("coffee")
+            MoneyUSE = MENU["cappuccino"]["cost"]
+        else:
+            print("Invalid coffee type! Try again.")
+            exit()
+
+        check_resources(WaterUSE, MilkUSE, CoffeeUSE, MoneyUSE)
 
 #TODO Check resources sufficient?
-def check_resources():
-    if resources["water"] < WaterUSE:
+def check_resources(Water, Milk, Coffee, Money):
+    if resources["water"] < Water:
         print("Sorry there is not enough water.")
-    elif resources["milk"] < MilkUSE:
+        exit()
+    elif resources["milk"] < Milk:
         print("Sorry there is not enough milk.")
-    elif resources["coffee"] < CoffeeUSE:
+        exit()
+    elif resources["coffee"] < Coffee:
         print("Sorry there is not enough coffee.")
-
+        exit()
+    else:
+        resources["water"] -= Water 
+        resources["milk"] -= Milk 
+        resources["coffee"] -= Coffee
+        process_coins(total=Money)
 
 #TODO Process coins.
 '''
@@ -61,28 +72,27 @@ Dimes: $0.10
 Nickles: $0.05
 Pennies: $0.01
 '''
-def process_coins():
+def process_coins(total):
+    global coffee_type
     print("Please insert coins.")
     quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
     nickles = int(input("How many nickles?: "))
     pennies = int(input("How many pennies?: "))
-    total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
-    return total
-
-
-#TODO Check transaction successful?
-def check_transaction():
-    if total >= MoneyUSE:
-        change = total - MoneyUSE
+    recived_money = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+    #TODO Check transaction successful?
+    if recived_money >= total:
+        resources["money"] += total
+        change = recived_money - total
         print(f"Here is ${change} in change.")
-        print(f"Here is your {coffee_type} ☕️. Enjoy!")
+        print(resources)
+        #TODO Fix the how to refer to coffee_type in function start_order
+        print(f"Here is your {coffee_type}. Enjoy!")
     else:
         print("Sorry that's not enough money. Money refunded.")
 
+    start_order()
 
 #TODO Make Coffee.
-
-
 
 start_order()
