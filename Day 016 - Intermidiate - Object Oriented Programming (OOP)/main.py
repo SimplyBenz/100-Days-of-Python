e35428b11b090b33d4f009d1459e98b90c989910
Tanaratta Umpon <tanaratta@gmail.com>
2025-01-07 06:56:38 +0700

    Day 16 of python

--- Day 016 - Intermidiate - Object Oriented Programming (OOP)/main.py
+++ Day 016 - Intermidiate - Object Oriented Programming (OOP)/main.py
@@ -1,3 +1,26 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# CoffeeMenuItem = MenuItem()
+CoffeeMenu = Menu()
+CoffeeMachine = CoffeeMaker()
+CoinMachine = MoneyMachine()
+
+IsOn = True
+
+while IsOn:
+
+    OrderCoffee = input(f"What would you like? ({CoffeeMenu.get_items()}): ")
+
+    if OrderCoffee == "off":
+        print("Turning off the coffee machine.")
+        IsOn = False
+    elif OrderCoffee == "report":
+        CoffeeMachine.report()
+        CoinMachine.report()
+    else:
+        drink = CoffeeMenu.find_drink(OrderCoffee)
+        if CoffeeMachine.is_resource_sufficient(drink):
+            if CoinMachine.make_payment(drink.cost):
+                CoffeeMachine.make_coffee(drink)
